ce51a23dad7c4afb984087e2df28d8e49586e1b5
임수빈 <hange253@gmail.com>
2022-10-06 09:56:40 +0900

    [UPDATE] App.js
    router 경로 설정 Home > Main

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
+import Main from "./components/Main";
 import Modal from "./components/Modal";
-import Home from "./page/Home";
 import MyPage from "./page/MyPage";
 import SignUpPage from "./page/SignUpPage";
 export default function App() {
@@ -9,11 +9,10 @@ export default function App() {
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Main />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/test" element={<Modal />}></Route>
         <Route path="/signup" element={<SignUpPage />}></Route>
-        
       </Routes>
     </>
   );

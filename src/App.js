68a8b88dbf77554baeb270b9c5729ce352bf1327
DoHi0512 <donghuni512@gmail.com>
2022-10-06 17:33:18 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import Header from "./components/common/Header";
-=======
->>>>>>> 86b34ef526b33c41088028bf07d72a2747434de1
 import Main from "./components/Main";
 import MyPage from "./page/MyPage";
 import SignUpPage from "./page/SignUpPage";
 import LoginPage from "./page/LoginPage";
 import CategoryPage from "./page/CategoryPage";
 import Game from "./sfdgs4/Game";
+import Header from "./components/common/Header";
 export default function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <Header />
-=======
     <>
->>>>>>> 86b34ef526b33c41088028bf07d72a2747434de1
+      <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
         <Route path="/mypage" element={<CategoryPage />}></Route>
@@ -25,6 +18,6 @@ export default function App() {
         <Route path="/board" element={<MyPage />} />
         <Route path="/game" element={<Game />}></Route>
       </Routes>
-    </div>
+    </>
   );
 }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,29 +2,29 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { CateState, modalState } from "..";
+import { modalState } from "..";
 
 export default function Modal() {
   const [modal, setModal] = useRecoilState(modalState);
-  const [cate, setCate] = useRecoilState(CateState);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   document.body.style.overflow = "hidden";
   function onSub(e) {
+    console.log("게시글 등록")
     e.preventDefault();
     const form = {
       title: title,
       content: content,
       id: localStorage.getItem("id"),
       date: "2022/05/12",
-      categorie: cate,
+      categorie: localStorage.getItem("cate"),
     };
     console.log(form);
     axios
       .post("http://192.168.72.124:8080/addboard", form)
       .then((res) => {
         console.log(res);
-        document.location.href = "/board"
+        document.location.href = "/board";
       })
       .catch((err) => {
         console.log(err);
@@ -45,7 +45,7 @@ export default function Modal() {
           </button>
         </Head>
         <Section>
-          <Content onSubmit={onSub}>
+          <Content >
             <input
               type="text"
               placeholder="제목을 입력해주세요!"
@@ -58,7 +58,7 @@ export default function Modal() {
               placeholder="내용을 입력해주세요"
               onChange={(e) => setContent(e.target.value)}
             ></textarea>
-            <button type="submit">글쓰기</button>
+            <button type="submit" onClick={onSub}>글쓰기</button>
           </Content>
         </Section>
       </Popup>
@@ -66,11 +66,80 @@ export default function Modal() {
   );
 }
 const StyledModal = styled.div`
-  box-shadow: 3px 3px 1000px 1000px rgba(0, 0, 0, 0.5);
-  top: 190px;
-  position: absolute;
   z-index: 2;
-  background-color: #ede4cc;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #20202060;
+`;
+
+const Head = styled.div`
+  position: relative;
+  width: 100%;
+  height: 70px;
+  border-bottom: 1px solid #000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    font-size: 18px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  button {
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    border-radius: 20px;
+    width: 40px;
+    height: 40px;
+    border: none;
+    background-color: #bac8ff;
+    color: #4263eb;
+    cursor: pointer;
+  }
+`;
+
+const Section = styled.div``;
+
+const Content = styled.div`
+  input {
+    width: 600px;
+    height: 40px;
+    margin-top: 30px;
+    padding-left: 10px;
+    border-radius: 10px;
+    border: 1px solid #979797;
+    outline: none;
+  }
+  textarea {
+    width: 590px;
+    padding: 10px;
+    margin-top: 30px;
+    border-radius: 10px;
+    border: 1px solid #979797;
+    outline: none;
+    resize: none;
+  }
+  button {
+    margin-top: 25px;
+    width: 200px;
+    height: 40px;
+    border-radius: 20px;
+    border: none;
+    font-size: 14px;
+    font-weight: 600;
+    color: #fff;
+    background: #4263eb;
+    cursor: pointer;
+  }
 `;
 
 const Popup = styled.div`
@@ -79,7 +148,6 @@ const Popup = styled.div`
   box-shadow: 1px 1px 2px 2px #979797;
   border-radius: 20px;
   background: #fff;
-
   form {
     display: flex;
     justify-content: center;
